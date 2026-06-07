69661d5c669b882d158274a10c839d3718b923c8
syarinsuherman2-cpu <syarinsuherman2@gmail.com>
2026-06-07 15:28:02 +0700

    menambahkan app planner

---
+++ Simulator Perencana Perjalanan & Navigasi Wisata (Smart Travel Planner)/Applaner.py
@@ -0,0 +1,20 @@
+# =============================================================
+# APP PLANNER
+# =============================================================
+class AppPlanner:
+    def __init__(self):
+        self.db = []
+
+        self.stack_undo = []
+        self.queue_tiket = []
+
+        self.sll = ItinerarySLL()
+        self.dll = PhotoDLL()
+        self.cll = SlideshowCLL()
+
+        self.graph = TravelGraph()
+        self.hash_tix = TicketHash()
+
+        self.root_cat = CategoryTree("Wisata")
+
+        self.kategori_terdaftar = {}
